175069bb08abe2dec02f5e6f44326ad13158bc2f
TeckFreak <rahulvashistha96@gmail.com>
2021-07-21 14:43:46 +0530

    Added configuration

---
+++ app/src/config.ts
@@ -0,0 +1,25 @@
+export const appConfig = {
+    mainImage: "assets/images/banner.jpg",
+    touchPoints: [
+        {
+            position: { x: 0, y: 0 },
+            image: ""
+        },
+        {
+            position: { x: 0, y: 0 },
+            image: ""
+        },
+        {
+            position: { x: 0, y: 0 },
+            image: ""
+        },
+        {
+            position: { x: 0, y: 0 },
+            image: ""
+        },
+        {
+            position: { x: 0, y: 0 },
+            image: ""
+        },
+    ]
+}
