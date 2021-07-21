17b308aa7d097f9a66da0ee531f29777a30352e9
TeckFreak <rahulvashistha96@gmail.com>
2021-07-21 14:44:15 +0530

    Added image movement sample

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,20 @@
-document.getElementById("test")!.innerText = "Hello";
+import { appConfig } from "./config";
+
+const image = document.createElement('img') as HTMLImageElement;
+image.src = appConfig.mainImage;
+image.className = "main-image";
+
+document.body.appendChild(image);
+
+let i = 0;
+
+const timer = setInterval(() => {
+    console.log(image.width, window.innerWidth)
+    if (i <= image.width - window.innerWidth) {
+        image.style.transform = `translateX(${-i}px)`;
+        i++;
+    }
+    else {
+        clearInterval(timer);
+    }
+}, 1);
