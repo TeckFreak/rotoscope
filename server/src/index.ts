8d96bea5d2a6d7a212c24af13aca46de062adef7
TeckFreak <rahulvashistha96@gmail.com>
2021-07-23 23:29:42 +0530

    Added serial manager

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,30 @@
 import Server from "./server";
+import { SerialManager } from "./services/serial-manager";
 import SocketManager from "./services/socket-manager";
 
 class Application {
     private server: Server;
     private socketManager: SocketManager;
+    private serialManager: SerialManager;
 
     private static application: Application;
 
     private constructor() {
         this.server = new Server();
         this.socketManager = new SocketManager(this.server.io);
+        this.serialManager = new SerialManager(this.socketManager);
     }
 
     public startServer(): void {
         this.socketManager.initSocket();
+        this.serialManager.initSerial();
 
         this.server.app.listen(3000, (err: Error, address: string) => {
             if (err) {
                 throw err;
             }
 
-            console.log(`👉 Server started on ${address} 🚀`);
+            console.log(`🟢 Server started on ${address} 🚀`);
         });
     }
 
---
+++ server/src/services/serial-manager.ts
@@ -0,0 +1,47 @@
+import SocketManager from "./socket-manager";
+import SerialPort from "serialport";
+import { ISocketData } from "../interfaces/ISocketData";
+
+export class SerialManager {
+    private serial?: SerialPort;
+    private parser?: SerialPort.parsers.Readline;
+
+    constructor(private socket: SocketManager) {
+    }
+
+    public initSerial(): void {
+        this.serial = new SerialPort("/dev/ttyS0", {
+            baudRate: 9600,
+            dataBits: 8,
+            parity: "none",
+            stopBits: 1,
+        }, (error: Error | null | undefined) => {
+            if(error) {
+                console.error('🔴', error);
+            }
+        });
+
+        this.parser = new SerialPort.parsers.Readline({
+            delimiter: "\r\n",
+            encoding: "utf8"
+        });
+
+        this.serial.pipe(this.parser);
+
+        this.serial.on("open", this.onSerialOpen.bind(this));
+    }
+
+    private async onSerialOpen() {
+        this.parser!.on('data', this.onParserDataReceived.bind(this));
+
+        await new Promise(done => setTimeout(done, 1000));
+
+        console.log("👉 Board Connected");
+    }
+
+    private onParserDataReceived(data: any) {
+        console.log(data);
+
+        this.socket.sendData(data as ISocketData);
+    }
+}
