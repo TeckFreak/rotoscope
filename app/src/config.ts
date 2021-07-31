5730bf9ef1497795b9845df1d2aaad0b9df71cfc
TeckFreak <rahulvashistha96@gmail.com>
2021-07-31 11:36:26 +0530

    Moved from image to file

--- app/src/config.ts
+++ app/src/config.ts
@@ -2,24 +2,24 @@ export const appConfig = {
     mainImage: "assets/images/banner.jpg",
     touchPoints: [
         {
-            position: { x: 0, y: 0 },
-            image: ""
+            position: { x: 6300, y: 810 },
+            file: "assets/videos/video.mp4"
         },
         {
             position: { x: 0, y: 0 },
-            image: ""
+            file: ""
         },
         {
             position: { x: 0, y: 0 },
-            image: ""
+            file: ""
         },
         {
             position: { x: 0, y: 0 },
-            image: ""
+            file: ""
         },
         {
             position: { x: 0, y: 0 },
-            image: ""
+            file: ""
         },
     ],
     totalCM: 200
