e1fe62f2ba6c9e65c1c6acc6b153bf72b61b2e7d
TeckFreak <rahulvashistha96@gmail.com>
2021-07-31 11:36:37 +0530

    Added buttons and video support

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,6 +3,11 @@ import { io } from 'socket.io-client';
 import { ISocketData } from "./ISocketData";
 
 let lastDistance = 0;
+let image: HTMLImageElement;
+let container: HTMLSpanElement;
+let i = 0;
+let modal: HTMLDivElement;
+let modalContent: HTMLDivElement;
 
 const socket = io("localhost:3000");
 socket.on('connect', () => {
@@ -10,29 +15,122 @@ socket.on('connect', () => {
 });
 
 socket.on('DISTANCE_UPDATE', (data: ISocketData) => {
-    if(data.distance > lastDistance + 2 || data.distance < lastDistance - 2){
+    if (data.distance > lastDistance + 2 || data.distance < lastDistance - 2) {
         moveBody(data.distance);
     }
 });
 
-const image = document.createElement('img') as HTMLImageElement;
-image.src = appConfig.mainImage;
-image.className = "main-image";
-
-document.body.appendChild(image);
-
-let i = 0;
-
 function moveBody(distance: number) {
     const moveTo = ((image.width - window.innerWidth) - distance * getPixelsPerCentimeter());
 
-    i = image.x;
-    while(i > -moveTo) {
-        image.style.transform = `translateX(${i}px)`;
-        i--;
+    i = getOffset(container).left;
+    if (i > -moveTo) {
+        while (i > -moveTo) {
+            container.style.left = `${i}px`;
+            i--;
+        }
+    }
+    else if (i < -moveTo && i < 1) {
+        while (i < -moveTo && i < 1) {
+            container.style.left = `${i}px`;
+            i++;
+        }
     }
 }
 
 function getPixelsPerCentimeter(): number {
     return (image.width - window.innerWidth) / appConfig.totalCM;
 }
+
+function getButtonLocation(position: { x: number, y: number }): { x: number, y: number } {
+    const widthDecrease = image.width / image.naturalWidth;
+    const heightDecrease = image.height / image.naturalHeight;
+
+    return {
+        x: position.x * widthDecrease,
+        y: position.y * heightDecrease
+    };
+}
+
+function initImage(): void {
+    image = document.createElement('img') as HTMLImageElement;
+    image.src = appConfig.mainImage;
+    image.className = "main-image";
+
+    container.appendChild(image);
+}
+
+function initContainer(): void {
+    container = document.createElement('div') as HTMLDivElement;
+    container.className = 'container';
+
+    document.body.append(container);
+}
+
+function initButtons(): void {
+    appConfig.touchPoints.forEach((touchPoint) => {
+        const button = document.createElement('button') as HTMLButtonElement;
+        const position = getButtonLocation(touchPoint.position);
+        button.style.left = `${position.x}px`;
+        button.style.top = `${position.y}px`;
+
+        // button.addEventListener('click',)
+        button.onclick = () => {
+            showModal(touchPoint.file);
+        }
+
+        container.appendChild(button);
+    });
+}
+
+function initModal(): void {
+    modal = document.createElement('div');
+    modal.className = 'modal hide';
+
+    modalContent = document.createElement('div');
+    modal.className = 'modal-content';
+
+    document.body.append(modal);
+    modal.appendChild(modalContent);
+}
+
+function showModal(file: string): void {
+    const video = document.createElement('video') as HTMLVideoElement;
+    video.style.width = "100%";
+    video.style.height = "100%";
+
+    const source = document.createElement('source') as HTMLSourceElement;
+    source.src = file;
+    source.type = 'video/mp4';
+
+    video.append(source);
+    modalContent.appendChild(video);
+
+    modal.className = 'modal show';
+    video.play();
+
+    modal.onclick = () => {
+        hideModal();
+    }
+}
+
+function hideModal(): void {
+    modalContent.lastChild && modalContent.removeChild(modalContent.lastChild);
+    modal.className = 'modal hide';
+}
+
+function getOffset(el: HTMLElement) {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+}
+
+initContainer();
+initImage();
+initModal();
+
+window.onload = () => {
+    initButtons();
+};
